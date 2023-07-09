eca099f290a18fd8707e801a0a58fd4b1de1045d
Patel Smit <sp5465437@gamil.com>
2023-07-09 09:58:36 +0530

    Initial commit

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"travel.management.system","l":"Conn"},{"p":"travel.management.system","l":"Login"},{"p":"travel.management.system","l":"Signup"},{"p":"travel.management.system","l":"Splash"}];updateSearchResults();
---
+++ src/travel/management/system/Conn.java
@@ -0,0 +1,18 @@
+package travel.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql:///travel_management_system","root","Nares12n@");
+            s=c.createStatement();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
