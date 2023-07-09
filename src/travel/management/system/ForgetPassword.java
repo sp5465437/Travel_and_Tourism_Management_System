f9acf0e60ec7da42ee5522a93988593ed3a0ea14
Patel Smit <sp5465437@gmaiil.com>
2023-07-09 12:52:41 +0530

    Initial commit

--- src/travel/management/system/ForgetPassword.java
+++ src/travel/management/system/ForgetPassword.java
@@ -16,7 +16,7 @@ public class ForgetPassword extends JFrame implements ActionListener{
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/forgotpassword.jpg"));
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/forgotpassword.png"));
         Image i2=i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
         JLabel image=new JLabel(i3);
